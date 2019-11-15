b56d4c2df509d0eb71d522930f9218317e3a0e89
kepka <kepka.max.ua@ukr.net>
2019-11-15 23:56:52 +0200

    feat(data-input): add functional from correct input num of teamand name

--- js/index.js
+++ js/index.js
@@ -1,8 +1,42 @@
 
+let team = [];
+let numOfTeam = 0;
+let getNumberToggle = true;
 
-function getNames(){
-    const input = document.querySelector('.input-name');
-    const name = input.value;
-    console.log(name);
-    input.value = '';
+function getNumberOfTeam(){
+    const numInput = findElement('#input-num');
+    const nameInput = findElement('#input-name');
+    numOfTeam = +numInput.value;
+    visibilityContainersToggle();
+    getNumberToggle = false;
+}
+
+function getName(){
+    const nameInput = findElement('#input-name');
+    const name = nameInput.value;
+    team.push(name);
+    nameInput.value = '';
+}
+
+function findElement(selector){
+    return document.querySelector(selector);
+}
+
+function visibilityElementToggle(element){
+    element.classList.toggle('display-none');
+}
+
+function visibilityContainersToggle(){
+    const numContainer = findElement('#num-container');
+    const nameContainer = findElement('#name-container');
+    visibilityElementToggle(numContainer);
+    visibilityElementToggle(nameContainer);
+}
+
+function checkValue(element){
+    return element.value === '' ? false : true;
+}
+
+function getNameToggle(){
+    getNumberToggle ? getNumberOfTeam() : getName();
 }
